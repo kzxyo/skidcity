851a8e56e240b1f338e472ccb167b7312d647af7
unleashed <08050603d@gmail.com>
2024-11-13 10:02:26 +0100

    lol

--- blair new/config.py
+++ blair new/config.py
@@ -6,20 +6,18 @@ __all__: Tuple[str, ...] = (
     "OWNER_IDS",
 )
 
-TOKEN: str = "OTEyMjY4MzU4NDE2NzU2ODE2.GKVyfs.wB38bR_e-UF3P1gDGIuXEE7j3CX4EJaOPAoM98"
-PUBLIC_KEY: str = "8f7ecf0f061b6043297290d3af428e05952be0ddd62eacb1d1e5a12374120938"
-APPLICATION_ID: int = 1285774777537007697
+TOKEN: str = "yeah"
+PUBLIC_KEY: str = "yeah"
+APPLICATION_ID: int = yeah
 PREFIX: str = ","
-SUPPORT: str = "https://discord.gg/blairbot"
+SUPPORT: str = "https://discord.gg/bleed"
 OWNER_IDS: List[int] = [
-    1083861728380584038,
-    1114247260474196098,
-    1107903478451408936,
+    1107903478451408936
 ]
 
 class Database:
     DSN: str = (
-        "postgresql://postgres.incpddplqzpbxmhdotvu:oOYk1rnF9RrathJQ@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
+        "yeah"
     )
 
 
